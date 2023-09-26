d732b14de7ceb7b1307cee98a7f67b0dfb26f61b
Yann BRONNER <yann.bronner@gmail.com>
2023-09-26 14:57:17 +0200

    Flake8

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from profiles.models import Profile
 
-# Sed placerat quam in pulvinar commodo. Nullam laoreet consectetur ex, sed consequat libero pulvinar eget. Fusc
-# faucibus, urna quis auctor pharetra, massa dolor cursus neque, quis dictum lacus d
+# Sed placerat quam in pulvinar commodo. Nullam laoreet consectetur ex, sed
+# consequat libero pulvinar eget. Fusc
+# faucibus, urna quis auctor pharetra, massa dolor cursus neque, quis dictum
+# lacus d
 
 
 def index(request):
@@ -11,8 +13,8 @@ def index(request):
     return render(request, 'profiles/index.html', context)
 
 # Aliquam sed metus eget nisi tincidunt ornare accumsan eget lac
-# laoreet neque quis, pellentesque dui. Nullam facilisis pharetra vulputate. Sed tincidunt, dolor id facilisis fringilla, eros leo tristique lacus,
-# it. Nam aliquam dignissim congue. Pellentesque habitant morbi tristique senectus et netus et males
+# laoreet neque quis, pellentesque dui. Nullam facilisis pharetra vulputate.
+# senectus et netus et males
 
 
 def profile(request, username):
