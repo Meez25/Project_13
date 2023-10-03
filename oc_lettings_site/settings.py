3f1789ba933f402749cd8f857374bf6ae667ab3a
Yann BRONNER <yann.bronner@gmail.com>
2023-10-04 00:56:26 +0200

    another try

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -29,12 +29,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", ]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
+    DEBUG = False
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
