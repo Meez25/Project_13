61511a430c0566981ba767425bd56ee9cd2285f5
Yann BRONNER <yann.bronner@gmail.com>
2023-10-03 21:15:09 +0200

    added localhost

--- doc/procedure.rst
+++ doc/procedure.rst
@@ -1,4 +1,4 @@
 Procédure de déploiement et gestion
 ===================================
 
-
+Le déploiement est automatique et est gérer par GitHub actions, dès qu'un modifier est apporté à la branche 
--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["project-13.onrender.com", ]
+ALLOWED_HOSTS = ["project-13.onrender.com", "localhost", ]
 
 
 # Application definition
